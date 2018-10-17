cfcea1c1075785955c080164ec19ec3e614f5eb1
jantotong <43649573+jantotong@users.noreply.github.com>
2018-10-16 22:33:01 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -24,46 +24,37 @@ int main()
 	listptr->Add(2, "Chain 2");
 	//display
 	cout << "First layer of Linkedlist" << endl;
-	cout <<  "Result: "  << listptr->getEntry(1) << endl;
+	cout << "Result: " << listptr->getEntry(1) << endl;
 	//second layer
 	cout << "Second layer of Linkedlist" << endl;
 	cout << "Result: " << listptr->getEntry(2) << endl;
 	//end of linkedlist
 
+	/*
 	//beggining of stack
 	Stack<string> *stackStringptr;
 	Stack<int> *stackIntptr;
 	Stack<Dollar*> *stackCurrency;
 
-	/*
-
+	//
 	stackIntptr = new Stack<int>();
 	stackStringptr = new Stack<string>();
 	stackCurrency = new Stack<Dollar*>();
-
-
 	cout << "Will now push A,B,C,D " << endl;
 	stackStringptr->push("a");
 	stackStringptr->push("b");
 	stackStringptr->push("c");
 	stackStringptr->push("d");
-
 	stackIntptr->push(1);
 	stackIntptr->push(2);
 	stackIntptr->push(3);
 	stackIntptr->push(4);
-
 	stackCurrency->push(new Dollar(0, 0));//start with 0
-
-
 	//Now printing stacks
-
 	cout << "Printing Stacks:" << endl;
-
 	stackStringptr->printStack();
 	stackIntptr->printStack();
 	stackCurrency->printStack();
-
 	cout << "Will show pop now:" << endl;
 	stackStringptr->pop();
 	stackStringptr->pop();
@@ -71,15 +62,12 @@ int main()
 	stackIntptr->pop();
 	stackIntptr->pop();
 	stackCurrency->pop();
-
 	cout << "Printing Stacks:" << endl;
 	stackStringptr->printStack();
 	stackIntptr->printStack();
 	stackCurrency->printStack();
-
 	//Crash code:
 	stackStringptr.getLength();
-
 	//remove everything
 	delete stackStringptr;
 	delete stackIntptr;
@@ -88,5 +76,6 @@ int main()
 	stackIntPointer = nullptr;
 	stackStringptr = nullptr;
 	*/
-	system ("pause");
-}
+	system("pause");
+	
+};
