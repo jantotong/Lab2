a4f44e76da438a1fb065546ba21bf5063f6dd1af
jantotong <43649573+jantotong@users.noreply.github.com>
2018-10-16 17:50:14 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,4 +11,22 @@ using namespace std;
 
 int main()
 {
+  //TODO
+  /*
+  
+  creat linkedList demonstrate addAnywhere
+  
+  create stack of ints, of strings, of Dollars
+        demonstrate push, pop, isempty stacktop,
+        after each step showstack
+   intStack.push(9)
+   intStack.show()
+   stringSTack.push('betty white')
+   stringStack.show()
+   
+   show that running a list function on a stack creates an error comment it out
+   //stack.showLast() ---error
+   
+   
+  */
 }
