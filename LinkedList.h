4fe43cdf0402765c6cfc554367ca0658a9f38158
jantotong <43649573+jantotong@users.noreply.github.com>
2018-10-20 16:44:51 -0700

    Update LinkedList.h

--- LinkedList.h
+++ LinkedList.h
@@ -1,244 +1,94 @@
-#ifndef Linkedlist_H
-#define Linkedlist_H
-using namespace std;
-
+#include <iostream>
+#include <string>
 #include "Node.h"
 #include "HeadNode.h"
+#include "Queue.h"
+#include "LinkedList.h"
+#include "Currency.h"
+using namespace std;
 
-template <class T>
-class Linkedlist
+//Group Members: Haddar Goldstein and Jan To Tong
+
+int main()
 {
-protected:
-	HeadNode<T> headNode;
+	cout << "Queue:" << endl;
+	Queue<string> *QueueStringptr;
+	Queue<int> *QueueIntptr;
+	Queue<Dollar*> *QueueDollar; //dollar address being pointed to
 
-	Node<T> *getNodeAt(int position) const
-	{
-		//Counting from the beginning of the chain
-		Node<T> *currentPointer = headNode.getNext(); //derived class contains parent's function
-		for (int i = 1; i < position; i++)
-		{
-			currentPointer = currentPointer->getNext();
-		}
-		return currentPointer;
-	}
+	QueueIntptr = new Queue<int>();
+	QueueStringptr = new Queue<string>();
+	QueueDollar = new Queue<Dollar*>();  //Queuedollar is a double pointer and refers to the address 
+	
+	//demonstrate enqueue
+	cout << "Enqueue Alpha, Beta, Charlie in string pointer " << endl;
+	QueueStringptr->enqueue("Alpha");
+	QueueStringptr->enqueue("Beta");
+	QueueStringptr->enqueue("Charlie");
 
-public:
-	//Constructor
-	Linkedlist()
-	{
-		headNode.setNext(nullptr);
-		headNode.setLast(nullptr);
-		headNode.setItemCount(0); //pre-setting everything to empty(null and zero)
-	}
+	cout << "Enqueue 1,2,3 in int pointer" << endl;
+	QueueIntptr->enqueue(1);
+	QueueIntptr->enqueue(2);
+	QueueIntptr->enqueue(3);
+	
+	cout << "Enqueue 5 Dollars 50 cents and 2 Dollars 40 cents in pointer" << endl;
+	QueueDollar->enqueue(new Dollar(5, 50));
+	QueueDollar->enqueue(new Dollar(2, 40)); 
 
-	//New list; using copy constructor
-	Linkedlist(const Linkedlist<T>& aList)
-	{
-		//pointer to node in original chain
-		Node<T>* Chainpointer = aList.headNode.getNext();
+	//Now printing Queues
+	cout << endl << "Printing Queues:" << endl;
+	QueueStringptr->printQueue();
+	QueueIntptr->printQueue();
+	QueueDollar->printQueue();
 
-		//item count
-		headNode.setItemCount(aList.headNode.getItemCount());
+	//demonstrate dequeue
+	cout << endl<< "Will dequeue each pointer once now:" << endl << endl;
+	QueueStringptr->dequeue();
+	QueueIntptr->dequeue();
+	QueueDollar->dequeue(); //will show nothing since only currency is dequeueped
+	
+	cout << "Printing Queues:" << endl;
+	QueueStringptr->printQueue();
+	QueueIntptr->printQueue();
+	QueueDollar->printQueue();
 
-		//if chain already exist and is empty
-		if (Chainpointer == nullptr)
-		{
-			//do nothing
-		}
+	//Will now enqueue to show enqueue adds at the rear
+	cout << endl << "Enqueued string 'Delta', int 4, and 6 Dollars 69 Cents" << endl;
+	QueueStringptr->enqueue("Delta");
+	QueueIntptr->enqueue(4);
+	QueueDollar->enqueue(new Dollar(6, 69));
+	cout << endl << "Printing Queues:" << endl;
+	QueueStringptr->printQueue();
+	QueueIntptr->printQueue();
+	QueueDollar->printQueue();
 
-		else
-		{
-			//copy first node
-			headNode.setNext(new Node<T>());
-			this->setEntry(1, Chainpointer->getItem());
+	//front and rear
+	cout << endl << "Will now display front and rear:" << endl<<endl<< "Front: " <<endl;
+	cout<< QueueStringptr->front() << endl;
+	cout << QueueIntptr->front() << endl; 
+	cout << QueueDollar->front() << endl << endl << "Rear: " << endl;
+	cout << QueueStringptr->rear() << endl;
+	cout << QueueIntptr->rear() << endl;
+	cout << QueueDollar->rear() << endl;
+	
+	//empty
+	cout << endl << "Will now display Empty:" << endl;
+	QueueStringptr->empty();
+	QueueIntptr->empty();
+	QueueDollar->empty();
+	QueueStringptr->printQueue();//shouldnt show anything
+	QueueIntptr->printQueue();
+	QueueDollar->printQueue();
 
-			//copy remaining nodes
-			Node<T>* newChainPointer = headNode.getNext();	//last-node pointer
+	//Crash code: (Code that will crash)
+	//QueueStringptr.getLength(); //Can't use linked list function with Queue object
 
-			for (int i = 1; i < headNode.getItemCount(); i++)
-			{
-				//pointing to next in chain
-				Chainpointer = Chainpointer->getNext();
-
-				//getting next item in chain
-				T nextItem = Chainpointer->getItem();
-
-				//creating new node for new item
-				Node<T>* newNodePointer = new Node<T>(nextItem);
-
-				//linking new node to end of new chain
-				newChainPointer->setNext(newNodePointer);
-
-				//
-				newChainPointer = newChainPointer->getNext();
-			}
-
-			//last chain = null
-			newChainPointer->setNext(nullptr);
-
-			//last chain pointer
-			headNode.setLast(newChainPointer);
-
-		}
-	}
-
-	//Destructor
-	~Linkedlist()
-	{
-		Empty();
-	}
-
-	//Checks if list is empty
-	bool isEmpty() const
-	{
-		return (headNode.getItemCount() == 0);
-	}
-
-	//Gets the length of the list
-	int getLength() const
-	{
-		return (headNode.getItemCount());
-	}
-
-	//Add anywhere
-	void Add(int newPosition, const T& newEntry)
-	{
-		//Check if item exist in position
-		bool PositionExist = (newPosition >= 1) && (newPosition <= (headNode.getItemCount()) + 1);
-
-		if (PositionExist)
-		{
-			//New node (The one being inserted)
-			Node<T>* newNodePointer = new Node<T>(newEntry);
-
-			//If the position is at the front of the chain
-			if (newPosition == 1)
-			{
-				newNodePointer->setNext(headNode.getNext());
-				headNode.setNext(newNodePointer);
-				headNode.setLast(getNodeAt(headNode.getItemCount()));
-			}
-
-			//Adding new node elsewhere in chain
-			else
-			{
-				//Finding node that will be before new node
-				Node<T>* previousPointer = getNodeAt(newPosition - 1);
-
-				//Add the new node after previousPointer's node
-				newNodePointer->setNext(previousPointer->getNext());
-				previousPointer->setNext(newNodePointer);
-			}
-
-			//Total amount of entries+1; used for future methods
-			headNode.setItemCount(headNode.getItemCount() + 1);
-
-			//Setting tail pointer
-			headNode.setLast(getNodeAt(headNode.getItemCount()));
-		}
-	}
-
-	//Adding to first of chain
-	void AddFirst(const T& newEntry)
-	{
-		return Add(1, newEntry);
-	}
-
-	//Adding to last of chain
-	void AddLast(const T& newEntry)
-	{
-		return Add(headNode.getItemCount() + 1, newEntry);
-	}
-
-	//Delete anywhere
-	void remove(int position)
-	{
-		//temp memoery Pointer for 
-		Node<T>* currentPointer = nullptr;
-
-		//Check if the node exist
-
-
-		if (position >= 1 && position <= headNode.getItemCount())
-		{
-			//If the node is at the beginning of chain
-			if (position == 1)
-			{
-				//pointer to node to be removed must be saved in some temporary pointer in order to remove memory allocated for removed node
-				currentPointer = headNode.getNext();
-				headNode.setNext(getNodeAt(2));
-			}
-
-			//Removing node elsewhere in chain
-			else
-			{
-				//Finding node before the node that is going to removed
-				Node<T>* previousPointer = getNodeAt(position - 1);
-
-				//Pointing to node to delete
-				currentPointer = previousPointer->getNext();
-
-				//Removing node by connecting previous node with next node
-				previousPointer->setNext(currentPointer->getNext());
-			}
-
-			//Removing memory allocated for removed node
-			currentPointer->setNext(nullptr);
-			delete currentPointer;
-			currentPointer = nullptr;
-
-			//Total entries -1; used for method count of items in the list
-			headNode.setItemCount(headNode.getItemCount() - 1);
-
-			//setting tail pointer
-			headNode.setLast(getNodeAt(headNode.getItemCount()));
-		}
-
-	}
-
-	//Removes first entry; used for pop
-	void removeFirst()
-	{
-		return remove(1);
-	}
-
-	//Removes last entry
-	void removeLast()
-	{
-		return remove(headNode.getItemCount());
-	}
-
-	//Empty the list
-	void Empty()
-	{
-		while (!isEmpty()) //while loop keeps deleting the first node
-		{
-			remove(1);
-		}
-	}
-
-	//Find anywhere
-	T getEntry(int position)
-	{
-		//Check if chain exist
-		bool PositionExist = (position >= 1) && (position <= headNode.getItemCount());
-
-		if (PositionExist)
-		{
-			Node<T>* entryNodePointer = getNodeAt(position);
-			return entryNodePointer->getItem();
-		}
-		else
-		{
-			string NoExist = "Position doesn't exist\n";
-			throw NoExist;
-		}
-	}
-
-	//Get last Entry
-	T getLastEntry()
-	{
-		return getEntry(headNode.getItemCount());
-	}
+	//remove everything because c++
+	delete QueueStringptr;
+	delete QueueIntptr;
+	delete QueueDollar;
+	QueueDollar = nullptr;
+	QueueIntptr = nullptr;
+	QueueStringptr = nullptr;
+	system("pause");
 };
-#endif
